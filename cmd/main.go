8f191ddce4bd1e1c57c048490df663b1a47ac1db
Jason <jason@zgwit.com>
2022-06-07 08:17:56 +0800

    定义结构

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/gob"
 	"flag"
 	activation_code "github.com/activation-code"
+	"os"
+	"time"
 )
 
 func main() {
@@ -14,4 +17,15 @@ func main() {
 	flag.Usage()
 
 	_ = activation_code.GenerateKeyPairFile("private.key", "public.key")
+
+	lic := &activation_code.Licence{
+		Product:   "iot-master",
+		ExpireAt:  time.Now(),
+		User:      "jason@zgwit.com",
+		Signature: "0231687983216798516546898",
+	}
+	file, _ := os.OpenFile("gob", os.O_CREATE, os.ModePerm)
+	enc := gob.NewEncoder(file)
+	_ = enc.Encode(lic)
+	file.Close()
 }
